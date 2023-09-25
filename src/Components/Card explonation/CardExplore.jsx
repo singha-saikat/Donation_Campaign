288a7c4e1222ecc5daa59e198f1975216e854f96
Saikat <sachinsingha7899@gmail.com>
2023-09-25 20:52:40 +0600

    clicked:donate ; item will be set to localstroage

--- src/Components/Card explonation/CardExplore.jsx
+++ src/Components/Card explonation/CardExplore.jsx
@@ -1,22 +1,54 @@
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const CardExplore = ({ card }) => {
-  const { image, title, text_color, price, description } = card;
+  const { image, title, text_color, price, description,Id } = card;
+
+  const handleDonate = () => {
+    toast.success('Your contribution will make a significant difference and positively impact the lives of those in need', {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+    });
+    const addToDonationPage = [];
+    const DonationItems = JSON.parse(localStorage.getItem("donation"));
+    if(!DonationItems){
+        addToDonationPage.push(card);
+        localStorage.setItem('donation',JSON.stringify(addToDonationPage));
+
+    }
+    else{
+        const isExist = DonationItems.find(card => card.Id == Id);
+        if(!isExist){
+            addToDonationPage.push(...DonationItems,card);
+            localStorage.setItem('donation',JSON.stringify(addToDonationPage));
+        }else{
+            console.log("error");
+        }
+    }
+  };
+
   return (
-    <div>
-      <div className="mt-8">
-        <img className="w-full h-[70vh]" src={image} alt="" />
-        <div className="pl-8 bg-[#0B0B0B80] py-4 ">
-          <button
-            className="text-white p-2 rounded "
-            style={{ background: text_color }}
-          >
-            Donate {price}
-          </button>
-        </div>
+    <div className="relative">
+      <img className="w-full h-[70vh]" src={image} alt="" />
+      <div
+        className="absolute bottom-44 left-0 w-full  px-8 py-4"
+        style={{ background: `rgba(11, 11, 11, 0.4)`, backdropFilter: `blur(1px)` }}
+      >
+        <button onClick={handleDonate} className="text-white p-2 border rounded" style={{background:text_color}}>
+          Donate {price}
+        </button>
       </div>
-      <div className="mb-16">
-        <h1 className="mt-8 text-2xl font-bold mb-4">{title}</h1>
+      <div className="mt-8">
+        <h1 className="text-2xl font-bold mb-4">{title}</h1>
         <p>{description}</p>
       </div>
+      <ToastContainer />
     </div>
   );
 };
